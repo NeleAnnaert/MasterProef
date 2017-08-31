16a5c5c75d3a23e04b7d4f3aa72bc3520d7579eb
NeleAnnaert <annaert.nele@gmail.com>
2017-08-31 04:53:46 +0200

    def

--- bin/testDetBed.cpp
+++ bin/testDetBed.cpp
@@ -3,18 +3,19 @@
  *  Author: Nele Annaert
  */
 
-#include "GetImages.h"
+#include "SGImages.h"
 #include "Detect.h"
 
 
 int main (int argc, char** argv)
 {
-  LibSeek::GetImages get;
+  LibSeek::SGImages get;
 	int teller;
 	int AANTFRAMES = 232;
   std::string path="./Masks/img";
 	std::string path_full;
 	bool result;
+	bool prev_result = true;
   cv::Mat img;
 	cv::Mat bed = get.getImage("./Bed/bed.pbm");
 	LibSeek::Detect det(bed);
@@ -23,5 +24,11 @@ int main (int argc, char** argv)
   	path_full = path + std::to_string(teller) + ".pbm";
 		img=get.getImage(path_full);
 		result = det.detectionHead(img);
-	}
+		if (false==result)
+		{
+			std::string r = det.checkBed(img);
+			std::cout<<result<<std::endl;
+	 }
+	 prev_result = result;
+	} 
 }
